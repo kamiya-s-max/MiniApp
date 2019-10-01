61ee566f9c409a7a3f291a7ffaaa4e98472a8d5f
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-10-01 21:16:55 +0900

    developed post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(text: params[:text])
+    redirect_to action: :index
+  end
+
+  private
+  def post_params
+    params.permit(:text, :user_id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
   
 end
